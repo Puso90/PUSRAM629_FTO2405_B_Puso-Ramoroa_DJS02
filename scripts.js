b3e6f569cd561ca353196f79a48d596a97158332
Puso90 <juwishbrands@gmail.com>
2024-09-06 12:40:31 +0200

    Done. Validation when values are missing, if statement

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
 const form = document.querySelector("[data-form]");
+const dividendForm = document.querySelector("[dividend]")
 const result = document.querySelector("[data-result]");
 
 
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  
+
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+  if (dividend === '' || divider === '') {
+    return result.innerText = "Division not performed. Both values are required in inputs. Try again";
+  }
+
   result.innerText = Math.floor(dividend / divider);
 });
