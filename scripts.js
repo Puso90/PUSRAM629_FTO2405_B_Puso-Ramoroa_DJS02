95b5476832cc3a97809440f74dbddb2313b2355a
Puso90 <juwishbrands@gmail.com>
2024-09-06 13:32:56 +0200

    error stack, not quite

--- scripts.js
+++ scripts.js
@@ -15,9 +15,27 @@ form.addEventListener("submit", (event) => {
   if (dividend === '' || divider === '') {
     return result.innerText = "Division not performed. Both values are required in inputs. Try again";
   } else if (divider < 1 || dividend === 0) {
-    console.log(new Error("Cannot divide by 0").stack)
+    console.error(error.stack);
     return result.innerText = "Division not performed. Invalid number provided. Try again"
+  } else if (!Number(dividend) || !Number(divider)) {
+    console.log(new Error("Cannot divide by 0").stack)
   }
 
   result.innerText = Math.floor(dividend / divider);
+
+
+
+
+
 });
+
+
+
+
+/*_______________________________________________________________________________________________________________
+
+
+//console.log(new Error("Cannot divide by 0").stack)
+
+
+______________________________________________________________________________*/
